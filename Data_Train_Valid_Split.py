250555d38ee4631c0619ba2d1e2af68f2e5c1792
Kartik Bansal <50640114+kartikb7@users.noreply.github.com>
2019-12-10 21:09:33 -0500

    Changed Directory names and added comments

--- Data_Train_Valid_Split.py
+++ Data_Train_Valid_Split.py
@@ -10,26 +10,30 @@ import os
 import re
 from shutil import copy
 
+# Viewing all the category names
 diff_categories = []
-pat = '^.*flat.*$'
+pat = '^.*flat.*$' # Matching images with flat in their name
 
-os.chdir(r'C:\Users\Kartik\Desktop\Study Material\E-commerce Tech\Project\Neuro-pic\Categorizing Project\Iterations\Denim_Sweaters Project')
-
+# Looping thru each product category in Raw Data
 folders = glob.glob('./Data/Raw Data/*')
 for folder in folders:
     diff_categories.append(folder.split('\\')[1])
     dest_train = './Data/Model Data/Train/'+folder.split('\\')[1]
-    dest_test = './Data/Model Data/Test/'+folder.split('\\')[1]
-    os.mkdir(dest_train)
-    os.mkdir(dest_test)
+    dest_valid = './Data/Model Data/Valid/'+folder.split('\\')[1]
+    os.mkdir(dest_train) # Making destination folder for the train data
+    os.mkdir(dest_valid) # Making destination folder for the validation data
     categories_contents = glob.glob(folder+'/*')
     count = 0
+    
+    # Looping thru each folder within category to count total usable images
     for category in categories_contents:
         folder_contents = glob.glob(category+'/*')
         for file in folder_contents:
             if re.search(pat, file)!= None:
                 count+=1
     copied = 0
+    
+    # Copying 80% images in train and 20% in valid folders
     for category in categories_contents:
         folder_contents = glob.glob(category+'/*')
         for file in folder_contents:
@@ -38,4 +42,4 @@ for folder in folders:
                 if copied < int(count*4/5):
                     copy(file, dest_train+'/Train_Pic'+str(copied)+'.jpg')
                 else:
-                    copy(file, dest_test+'/Test_Pic'+str(copied)+'.jpg')
+                    copy(file, dest_valid+'/Valid_Pic'+str(copied)+'.jpg')
